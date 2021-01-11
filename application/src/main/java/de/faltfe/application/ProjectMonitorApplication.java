2c36019638900fdd1fa2fd2e3cabc4fc1f134b5f
faltfe <13038138+faltfe@users.noreply.github.com>
2021-01-11 20:35:47 +0100

    Initial setup

---
+++ application/src/main/java/de/faltfe/application/ProjectMonitorApplication.java
@@ -0,0 +1,13 @@
+package de.faltfe.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectMonitorApplication.class, args);
+	}
+
+}
