4af4722675d192970e84249a936fb708582daac7
faltfe <13038138+faltfe@users.noreply.github.com>
2021-01-19 10:38:00 +0100

    Fix jpa test

--- vacation-monitor/src/test/java/de/faltfe/vacation/repositories/PersonRepositoryTest.java
+++ vacation-monitor/src/test/java/de/faltfe/vacation/repositories/PersonRepositoryTest.java
@@ -1,15 +1,12 @@
 package de.faltfe.vacation.repositories;
 
+import de.faltfe.vacation.config.EnableVacationJpaTest;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-@ExtendWith(SpringExtension.class)
-@DataJpaTest
+@EnableVacationJpaTest
 class PersonRepositoryTest {
 
     @Autowired
