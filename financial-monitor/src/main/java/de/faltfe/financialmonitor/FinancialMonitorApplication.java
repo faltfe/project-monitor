5c9e5828ef534e413fe049afae86aafe572c2e1d
faltfe <13038138+faltfe@users.noreply.github.com>
2021-02-14 16:47:35 +0100

    Add new module

---
+++ financial-monitor/src/main/java/de/faltfe/financialmonitor/FinancialMonitorApplication.java
@@ -0,0 +1,13 @@
+package de.faltfe.financialmonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialMonitorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinancialMonitorApplication.class, args);
+    }
+
+}
---
+++ financial-monitor/src/test/java/de/faltfe/financialmonitor/FinancialMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package de.faltfe.financialmonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialMonitorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
