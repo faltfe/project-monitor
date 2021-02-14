81a6d42137da019ba2e773b66f49d4d13f9c51b2
faltfe <13038138+faltfe@users.noreply.github.com>
2021-02-14 18:17:07 +0100

    Enable and configure client discovery

--- financial-monitor/src/main/java/de/faltfe/financialmonitor/FinancialMonitorApplication.java
+++ financial-monitor/src/main/java/de/faltfe/financialmonitor/FinancialMonitorApplication.java
@@ -2,8 +2,10 @@ package de.faltfe.financialmonitor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class FinancialMonitorApplication {
 
     public static void main(String[] args) {
--- gateway/src/main/java/de/faltfe/gateway/GatewayApplication.java
+++ gateway/src/main/java/de/faltfe/gateway/GatewayApplication.java
@@ -2,8 +2,11 @@ package de.faltfe.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class GatewayApplication {
 
     public static void main(String[] args) {
