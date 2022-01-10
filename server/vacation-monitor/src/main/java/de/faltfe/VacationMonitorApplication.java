2cd9742bd704742dd5a047df14ad9899d7f76043
faltfe <13038138+faltfe@users.noreply.github.com>
2022-01-11 00:12:51 +0100

    Rename file

--- server/vacation-monitor/src/main/java/de/faltfe/VacationMonitorApplication.java
+++ server/vacation-monitor/src/main/java/de/faltfe/VacationMonitorApplication.java
@@ -3,13 +3,15 @@ package de.faltfe;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class ProjectMonitorApplication {
+@EnableFeignClients
+public class VacationMonitorApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProjectMonitorApplication.class, args);
+		SpringApplication.run(VacationMonitorApplication.class, args);
 	}
 
 }
